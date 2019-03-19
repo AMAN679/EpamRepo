67498711ddad1b7eebf050f8cd34bb0ff254f973
AMAN KUMAR SINGH <39581790+AMAN679@users.noreply.github.com>
2019-03-19 19:37:22 +0530

    Add files via upload

---
+++ EpamTask/src/Calculator/Calculator.java
@@ -0,0 +1,19 @@
+package Calculator;
+
+class Calculator
+{
+	public int sumOfTwoNum(int num1,int num2)
+	{
+		return (num1+num2);
+	}
+	
+	public int multiplicationOfTwoNum(int num1,int num2)
+	{
+		return (num1*num2);
+	}
+	
+	public int divisonOfNum(int num1,int num2)
+	{
+		return (num1/num2);
+	}
+}
